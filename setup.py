ea60414a669947301ddb825b3b7cf14ba3fa4be3
AdelantaAngie <angie.paredes@adelantafactoring.com>
2025-12-05 10:27:35 -0500

    fix 1.0.16

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ else:
 
 setup(
     name="adelanta-toolbox",
-    version="1.0.15",
+    version="1.0.16",
     author="Jimmy",
     author_email="",  # Agrega tu email aquí
     description="Librería con arquitectura hexagonal para procesos financieros ETL",
